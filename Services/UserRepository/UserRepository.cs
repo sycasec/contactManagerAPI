4230e644819dd3e586fa5492e8eeed6eb8ac8a88
sycasec <sycasec@arch.btw>
2023-08-22 07:29:52 +0800

    added organization

--- Services/UserRepository/UserRepository.cs
+++ Services/UserRepository/UserRepository.cs
@@ -1,42 +1,7 @@
 using contactManagerAPI.DTO;
-using contactManagerAPI.Models;
+using contactManagerAPI.Data;
 
 namespace contactManagerAPI.Services.UserRepository
 {
-    public class UserRepository : IUserRepository
-    {
-        public async Task<User?> GetUserByUsername(string username)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<User?> GetUserByEmail(string email)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<bool> CreateUser(UserDTO req)
-        {
-            User request;
-            request.ID =
-        }
-        public async Task<bool> DeleteUserByUsername(string username)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<bool> DeleteUserByID(int ID)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<bool> UpdateUser(UserDTO req)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<bool> UpdateUserByID(int ID, User user)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task<IEnumerable<User>> GetAllUsers()
-        {
-            throw new NotImplementedException();
-        }
-    }
+
 }
